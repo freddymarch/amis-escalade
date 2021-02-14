179a4e047fafdda886380d81034bc5d82279f0ea
Eric LEGBA <eric.legba@gmail.com>
2021-02-14 23:54:09 +0100

    Amélioration du code existant

--- src/main/resources/sql/01_create_user.sql
+++ src/main/resources/sql/01_create_user.sql
@@ -5,5 +5,5 @@ create table T_USER
     C_NAME     varchar(50)  null,
     C_LASTNAME varchar(100) null,
     C_EMAIL    varchar(50)  null,
-    C_PASSWORD varchar(50)  null
+    C_PASSWORD varchar(255)  null
 );
--- src/main/resources/sql/08_create_commentaire.sql
+++ src/main/resources/sql/08_create_commentaire.sql
@@ -1,9 +1,9 @@
-create table t_commentaire
+create table T_COMMENTAIRE
 (
     C_ID          int auto_increment
         primary key,
-    C_DATE        date         null,
+    C_DATE        varchar(10)         null,
     C_COMMENTAIRE varchar(500) null,
     C_ID_SITE     int          null,
     C_ID_USER     int          null
-    );
+);
